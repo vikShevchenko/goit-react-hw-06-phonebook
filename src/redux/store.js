bcc886a2a33be3ea4822365a38edb7eff6aeb451
vikShevchenko <63931417+vikShevchenko@users.noreply.github.com>
2023-08-20 15:39:03 +0300

    persistor

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,40 @@
-import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./userSlise";
 
-export const store = configureStore({
-    reducer: {
-        user: userSlice
-    }
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const rootReducer = combineReducers({
+    user: userSlice,
+
 })
+
+const persistConfig = {
+    key: 'root',
+    storage: storage
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
+
+})
+
+export const persistor = persistStore(store)
+export default store
